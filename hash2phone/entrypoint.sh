a747b1cea7ead9027a4e35d8267e27f5aa620c2b
Jonathan Lacson <lacson@vt.edu>
2019-08-03 10:05:49 -0400

    start apache in entrypoint

--- hash2phone/entrypoint.sh
+++ hash2phone/entrypoint.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+# start our web server
+apachectl -D FOREGROUND &
+
 # start DB
-/usr/lib/postgresql/10/bin/pg_ctl -D /var/lib/postgresql/10/main -l /app/logfile start
+sudo -u postgres /usr/lib/postgresql/10/bin/pg_ctl -D /var/lib/postgresql/10/main -l /app/logfile start
 
 # config DB
-psql < /app/db_init.sql
+sudo -u postgres psql < /app/db_init.sql
 
 # run script
 python /app/hashmap_gen.py $1
